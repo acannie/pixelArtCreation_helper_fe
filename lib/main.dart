135474883239664ea72bb72605cea783ae83ea26
acannie <acannie0530@gmail.com>
2021-01-11 02:59:14 +0900

    code: made img upload system for web to copy OSS.

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,157 @@
 import 'package:flutter/material.dart';
+import 'dart:html' as html;
+import 'dart:typed_data';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http_parser/http_parser.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(UploadApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class UploadApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        fontFamily: 'Quicksand',
+        primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FileUploadApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class FileUploadApp extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  createState() => _FileUploadAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FileUploadAppState extends State<FileUploadApp> {
+  List<int> _selectedFile;
+  Uint8List _bytesData;
+  GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  startWebFilePicker() async {
+    html.InputElement uploadInput = html.FileUploadInputElement();
+    uploadInput.multiple = true;
+    uploadInput.draggable = true;
+    uploadInput.click();
+
+    uploadInput.onChange.listen((e) {
+      final files = uploadInput.files;
+      final file = files[0];
+      final reader = new html.FileReader();
+
+      reader.onLoadEnd.listen((e) {
+        _handleResult(reader.result);
+      });
+      reader.readAsDataUrl(file);
     });
+  }
+
+  void _handleResult(Object result) {
+    setState(() {
+      _bytesData = Base64Decoder().convert(result.toString().split(",").last);
+      _selectedFile = _bytesData;
+    });
+  }
+
+  Future<String> makeRequest() async {
+    var url = Uri.parse("http://localhost:5000/");
+    var request = new http.MultipartRequest("POST", url);
+    request.files.add(await http.MultipartFile.fromBytes(
+      'file',
+      _selectedFile,
+      contentType: new MediaType('application', 'octet-stream'),
+      filename: "file_up.jpg",
+    ));
+
+    request.send().then((response) {
+      print("test");
+      print(response.statusCode);
+      if (response.statusCode == 200) {
+        print("Uploaded!");
+      }
+    });
+
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        child: new AlertDialog(
+          title: new Text("Details"),
+          content: new SingleChildScrollView(
+            child: new ListBody(
+              children: <Widget>[
+                new Text("Upload successful"),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text('Aceptar'),
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => UploadApp()),
+                  (Route<dynamic> route) => false,
+                );
+              },
+            ),
+          ],
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('A Flutter Web file picker'),
+        ),
+        body: Container(
+          child: new Form(
+            autovalidate: true,
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 16.0, left: 28),
+              child: new Container(
+                  width: 350,
+                  child: Column(children: <Widget>[
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          MaterialButton(
+                            color: Colors.pink,
+                            elevation: 8,
+                            highlightElevation: 2,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8)),
+                            textColor: Colors.white,
+                            child: Text('Select a file'),
+                            onPressed: () {
+                              startWebFilePicker();
+                            },
+                          ),
+                          Divider(
+                            color: Colors.teal,
+                          ),
+                          RaisedButton(
+                            color: Colors.purple,
+                            elevation: 8.0,
+                            textColor: Colors.white,
+                            onPressed: () {
+                              makeRequest();
+                            },
+                            child: Text('Send file to server'),
+                          ),
+                        ])
+                  ])),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
